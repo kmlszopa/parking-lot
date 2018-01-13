3e581a735f54188d7ae10ead6366aca7944667f7
ksz <kmlszopa@gmail.com>
2018-01-14 00:06:15 +0100

    jakas dzialajaca wersja

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,10 @@ using namespace sf;
 const int windowHeight = 1000;
 const int windowWidth = 600;
 
-
+struct Target {
+    float x;
+    float y;
+};
 
 class Entity {
 public:
@@ -22,23 +25,19 @@ public:
         id  = id_generator++;
     }
 
-    void settings(Sprite &s, int X, int Y, float a=0) {
+    void settings(Sprite &s, float X, float Y, float a=0) {
         sprite = s;
         sprite.setOrigin(R,R);
         x = X;
         y = Y;
         angle = a;
-        R = 1;
+
 
     }
 
     virtual void update() {};
 
     void draw(RenderWindow &window) {
-        CircleShape circle(R);
-        circle.setFillColor(Color(255,0,0,170));
-        circle.setPosition(x,y);
-        circle.setOrigin(R,R);
         window.draw(sprite);
     }
 
@@ -48,14 +47,14 @@ public:
 class ParkingSpot: public Entity {
 public:
     bool free;
-    ParkingSpot(int X, int Y) {
+    ParkingSpot(float X, float Y) {
         x = X;
         y = Y;
         free = true;
     }
 
-    void changeFree(){
-        if(this->free){
+    void changeFree() {
+        if(this->free) {
             this->free = false;
         } else {
             this->free = true;
@@ -65,74 +64,115 @@ public:
 
 class Parking: public Entity {
 public:
-    int spotsCounter = 2;
+    Target exit = {250, 1200};
+    int spotsCounter = 10;
     std::vector<ParkingSpot*> spots;
     Parking() {
-        y = 110;
-        for(int i = 0; i < 10; i++) {
-            spots.push_back(new ParkingSpot(100, y+=60));
+        y = 696;
+        for(int i = 0; i < spotsCounter; i++) {
+            spots.push_back(new ParkingSpot(100, y-=58));
         }
     }
 };
 
 class Car: public Entity {
 public:
+    Clock clock;
+    float parkingTime;
+    ParkingSpot * parkingSpot;
     int n;
-    bool parked = 0;
+    float turnSpeed;
+    bool parked = 0, toExit = 0, left = 0;
+    Target target;
     Parking * parking;
+
     Car(Parking * parking) {
-        speed=5;
+        srand(time(NULL));
+        parkingTime = ((rand() % 10) + 5);
+        turnSpeed = 0.0075;
+        speed=3.5;
         angle=0;
         n=0;
         this->parking = parking;
+        this->parkingSpot = findFreeSpot();
+
     }
 
     ParkingSpot* findFreeSpot() {
-        for(int i = 0; i < parking->spots.size(); i++){
+        for(int i = 0; i < parking->spots.size(); i++) {
             ParkingSpot * tmp = parking->spots[i];
             if(tmp->free == true) {
+                tmp->free = 0;
                 ParkingSpot * parkingSpot = tmp;
                 return parkingSpot;
             }
         }
-        return new ParkingSpot(500, -200);
+        return new ParkingSpot(500, -10000000);
     }
 
     void park() {
-        ParkingSpot * parkingSpot = findFreeSpot();
         float tx=parkingSpot->x;
         float ty=parkingSpot->y;
 
         float beta = angle-atan2(tx-x,-ty+y);
-        if(y < ty+200) {
-            if (sin(beta)<0) angle+=0.005*speed;
-            else angle-=0.005*speed;
-            if ((x-tx)*(x-tx)+(y-ty)*(y-ty)<40*30) {
+        if(y < ty+127) {
+            if (sin(beta)<0) angle+=turnSpeed*speed;
+            else angle-=turnSpeed*speed;
+            if ((x-tx)*(x-tx)+(y-ty)*(y-ty)<40) {
                 n=(n+1)%parking->spotsCounter;
                 parked = 1;
-                parkingSpot->free = 0;
+                cout << parkingSpot->free << endl;
+                target.x = parking->exit.x+172;
+                target.y = ty;
+                clock.restart();
             }
         }
     }
 
-    void unPark(){
-        speed = -4;
+    void moveToExit() {
+        float tx=parking->exit.x;
+        float ty=parking->exit.y;
+        float beta = angle-atan2(tx-x,-ty+y);
+        if(y < ty+124) {
+            if (sin(beta)<0) angle+=turnSpeed*speed;
+            else angle-=turnSpeed*speed;
+        }
+
+        if(!left){
+            this->parkingSpot->free = 1;
+            left = true;
+        }
+
+    }
+
+    void unPark() {
+        Time time = clock.getElapsedTime();
+        speed = 0;
+        if(time.asSeconds() > parkingTime) {
+            speed = -3.5;
+        }
+        if(target.x-this->x <  30) {
+            this->toExit = 1;
+            speed = 3.5;
+        }
     }
 
     void move() {
-
         x += sin(angle) * speed;
         y -= cos(angle) * speed;
     }
 
     void update() {
-            if(!parked){
-                park();
-                move();
-            } else {
-                unPark();
-                move();
-            }
+        if(!parked) {
+            park();
+            move();
+        } else if(toExit) {
+            moveToExit();
+            move();
+        } else {
+            unPark();
+            move();
+        }
 
 
         sprite.setPosition(x, y);
@@ -143,27 +183,13 @@ public:
 
 int Entity::id_generator = 0;
 
-bool isColide(Entity* car, ParkingSpot* parkingSpot){
-    float carx = car->x;
-    float cary = car->y;
-    float px = parkingSpot->x - 20;
-    float py = parkingSpot->y;
-    //cout << "parkingSpot x: " << px << " y: " << py << endl;
-    //cout << "car x: " << carx << " y: " << cary << endl;
-    if(((carx>px)&&(carx<(px+100)))&&((cary>py)&&(cary<(py+60)))){
-        //cout << "true" << endl;
-        return true;
-    }
-    //cout << "false" << endl;
-    return false;
-}
-
 int main() {
-    Texture t1, t2, t3;
-    t1.loadFromFile("resources/background.png");
+    Texture  t2, t3;
+
     t2.loadFromFile("resources/car.png");
     t3.loadFromFile("resources/parking.png");
-    Sprite background(t1);
+    t2.setSmooth(true);
+
     Sprite carSprite(t2);
     Sprite parkingSprite(t3);
     bool Up=0,Right=0,Down=0,Left=0;
@@ -181,22 +207,17 @@ int main() {
     car->settings(carSprite, 500, 800);
 
 
-    float speed=0,angle=0;
-    float maxSpeed=3.0;
-    float acc=0.4, dec=0.3;
-    float turnSpeed=0.05;
+
     while(window.isOpen()) {
 
 
         Event event;
-        while (window.pollEvent(event))
-        {
+        while (window.pollEvent(event)) {
             if (event.type == Event::Closed)
                 window.close();
 
             if (event.type == Event::KeyPressed)
-                if (event.key.code == Keyboard::Space)
-                {
+                if (event.key.code == Keyboard::Space) {
                     Entity *sCar = new Car(parking);
                     entities.push_back(sCar);
                     sCar->settings(carSprite, 500, 800);
@@ -206,44 +227,8 @@ int main() {
             break;
         }
 
-        /*
-            bool Up=0,Right=0,Down=0,Left=0;
-            if (Keyboard::isKeyPressed(Keyboard::Up)) Up=1;
-            if (Keyboard::isKeyPressed(Keyboard::Right)) Right=1;
-            if (Keyboard::isKeyPressed(Keyboard::Down)) Down=1;
-            if (Keyboard::isKeyPressed(Keyboard::Left)) Left=1;
 
-            //car movement
-            if (Up && speed<maxSpeed)
-                if (speed < 0)  speed += dec;
-                else  speed += acc;
-
-            if (Down && speed>-maxSpeed)
-                if (speed > 0) speed -= dec;
-                else  speed -= acc;
-
-            if (!Up && !Down)
-                if (speed - dec > 0) speed -= dec;
-                else if (speed + dec < 0) speed += dec;
-                else speed = 0;
-
-            if (Right && speed!=0)  angle += turnSpeed * speed/maxSpeed;
-            if (Left && speed!=0)   angle -= turnSpeed * speed/maxSpeed;
-
-            car->speed = speed;
-            car->angle = angle;
-        */
-        for(auto a:entities){
-            for(ParkingSpot* ps:parking->spots){
-                if(isColide(a, ps)){
-                  //  ps->free = false;
-
-                }
-            }
-
-        }
-
-        for(auto i:entities){
+        for(auto i:entities) {
             i->update();
             i->draw(window);
         }
